80fbacf21db8722c01d675cf336b49e10be55250
ivanajenjo <ivan.ajenjo.vicente@gmail.com>
2021-04-13 21:10:13 +0200

    GridCreado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
-import 'screens/home/HomeScreen.dart';
+import 'widgets/plant_card.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,8 +17,20 @@ class MyApp extends StatelessWidget {
         textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(title: 'RiegApp'),
+      home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('RiegApp'),
+          ),
+          body: GridView.count(
+            crossAxisCount: 2,
+            children: [
+              PlantCard(),
+              PlantCard(),
+              PlantCard(),
+              PlantCard(),
+            ],
+          )),
     );
   }
 }
-
